9c18cfc28c028ba99743f00e70e9edede2407275
oliwiajanus <34365957+oliwiajanus@users.noreply.github.com>
2020-01-16 09:32:38 +0100

    Add files via upload

---
+++ kolokwium/CMakeFiles/myProgram.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/cisco/kolokwium/src/address.cpp" "/home/cisco/kolokwium/CMakeFiles/myProgram.dir/src/address.cpp.o"
+  "/home/cisco/kolokwium/src/employee.cpp" "/home/cisco/kolokwium/CMakeFiles/myProgram.dir/src/employee.cpp.o"
+  "/home/cisco/kolokwium/src/main.cpp" "/home/cisco/kolokwium/CMakeFiles/myProgram.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
